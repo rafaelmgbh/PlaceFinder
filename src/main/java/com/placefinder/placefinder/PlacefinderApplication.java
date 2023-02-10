64d280ace5a2eb7592326ebc260c6af4104bc14e
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2023-02-10 19:00:48 -0300

    setup initial , add spotbugs and checkstyle

---
+++ src/main/java/com/placefinder/placefinder/PlacefinderApplication.java
@@ -0,0 +1,14 @@
+package com.placefinder.placefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacefinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacefinderApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/placefinder/placefinder/BaseTest.java
@@ -0,0 +1,14 @@
+package com.placefinder.placefinder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BaseTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+        Assertions.assertTrue(true);
+    }
+
+}
